52d2020d3029468bfecd41d044bc3f361364371f
Eduardo.trabalho <eduardodepaulatrabalho2@gmail.com>
2025-09-03 20:10:25 -0300

    Conteúdo de Estudo JS avançado

---
+++ CursoDev-JS-avancado/Aula-03-Reduce/srcipt.js
@@ -0,0 +1,16 @@
+let ordens = [
+    {cliente: 'Roberto', tipo: 'compra', quantidade: 56, ativo: 'NFLX34'},
+    {cliente: 'RiCARDO', tipo: 'compra', quantidade: 76, ativo: 'AAPL34'},
+    {cliente: 'nda', tipo: 've', quantidade: 21, ativo: 'GOGL34'}
+]
+
+/* let quantidadeDeOrdens = 0
+for(let i = 0; i < ordens.length; i++){
+    quantidadeDeOrdens += ordens[i].quantidade
+} */
+
+/* let quantidadeDeOrdens = ordens.reduce(function(somaOrdens, ordem){
+    return somaOrdens + ordem.quantidade
+}, 0) */
+
+let quantidadeDeOrdens = ordens.reduce((somaOrdens, ordem) => somaOrdens + ordem.quantidade, 0)
