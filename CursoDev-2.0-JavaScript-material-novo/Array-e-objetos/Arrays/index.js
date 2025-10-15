84d74c58af6bc082b0259c89638bbc2721110921
Eduardo de Paula <eduardodepaulatrabalho2@gmail.com>
2025-10-14 21:42:15 -0300

    feat: Conteúdo JS - Arrays simples, bidimensional e tridimensional

---
+++ CursoDev-2.0-JavaScript-material-novo/Array-e-objetos/Arrays/index.js
@@ -0,0 +1,20 @@
+const filmes = ["star wars", "matrix", "mr. robot", "o preço do amanhã", "a vida é bela"];
+console.log(filmes);
+console.log(filmes[0]);
+
+//arrays bidimensionais
+const dados = ["Roberto", 23, {cidade: "São Paulo", estado: "SP"}];
+console.log(dados);
+
+
+//arrays multidimensionais
+const avaliacoesFilmes = [
+    ["star wars", 5],
+    ["matrix", 4.5],
+    ["mr. robot", 5],
+    ["o preço do amanhã", 4],
+    ["a vida é bela", 5]
+]
+
+console.log(avaliacoesFilmes[0][1]);
+
