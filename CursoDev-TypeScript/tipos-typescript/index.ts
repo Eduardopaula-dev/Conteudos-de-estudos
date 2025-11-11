d977c49b6b585cb159750839d96eba43a3d71fa5
Eduardo de Paula <eduardodepaulatrabalho2@gmail.com>
2025-11-10 21:42:13 -0300

    feat: Conteúdo TS - Outras tipagem no TypeScript

--- CursoDev-TypeScript/tipos-typescript/index.ts
+++ CursoDev-TypeScript/tipos-typescript/index.ts
@@ -17,4 +17,30 @@ function sendMessage(userName: string, isLogged: boolean) {
  const isLogged = true
  
  sendMessage(userName, isLogged)
- 
+
+
+//Array no JS
+const arrayJS = [1, 2, 3, 4, 5];
+
+//Array no TS
+const arrayTS: number[] = [1, 2, 3, 4, 5];
+//ou
+const arrayTS2: string[] = ["Eduardo", "Beto", "Ana"];
+
+//Any, tipo que aceita qualquer valor
+let valorAny: any = 5;
+valorAny = "Eduardo";
+valorAny = true;
+
+//Objetos em TypeScript
+function imprimirCoordenadas(coord: {x: number, y: number}) {
+    console.log("A coordenada x vale " + coord.x);
+    console.log("A coordenada x vale " + coord.y);
+} 
+
+imprimirCoordenadas({x: 29, y: 55})
+
+//ou
+
+let objeto: {x: number, y: number} = {x: 10, y: 20};
+console.log(objeto);
