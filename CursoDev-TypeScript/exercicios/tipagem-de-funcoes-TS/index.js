bfa8715ca1a9a695aec014a58954d3b90fa48f54
Eduardo de Paula <eduardodepaulatrabalho2@gmail.com>
2025-11-18 19:46:18 -0300

    feat: Exercício TS - Tipagem de funções em TypeScript

---
+++ CursoDev-TypeScript/exercicios/tipagem-de-funcoes-TS/index.js
@@ -0,0 +1,5 @@
+function calculo(numero1, numero2) {
+    return numero1 + numero2;
+}
+var resultado = calculo(232, 112);
+console.log("Resultado \u00E9: ".concat(resultado));
---
+++ CursoDev-TypeScript/exercicios/tipagem-de-funcoes-TS/index.ts
@@ -0,0 +1,6 @@
+function calculo(numero1: number, numero2: number): number {
+    return numero1 + numero2
+}
+
+const resultado = calculo(232, 112);
+console.log(`Resultado é: ${resultado}`);
