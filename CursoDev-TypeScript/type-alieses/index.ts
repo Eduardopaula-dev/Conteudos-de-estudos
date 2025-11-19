3ac3c59a36e02e5d12643f217963af0f16f4aebc
Eduardo de Paula <eduardodepaulatrabalho2@gmail.com>
2025-11-19 19:25:25 -0300

    feat: Conteúdo TS - Type aliases: dando apelidos aos nossos tipos

---
+++ CursoDev-TypeScript/type-alieses/index.ts
@@ -0,0 +1,23 @@
+type id = number | string;
+
+let idCustumer: id;
+
+idCustumer = 1211;
+idCustumer = "wdqgqqfw";
+
+let idProduct: id;
+
+idProduct = "Sapato";
+idProduct = 23.99;
+
+type id2 = number
+
+let idCustumer2: id2;
+
+idCustumer2 = 1211;
+idCustumer2 = "wdqgqqfw";
+
+let idProduct2: id2;
+
+idProduct2 = "Sapato";
+idProduct2 = 23.99;
