f1edfe2045b37ac00aaaba5f7ee4f43b1dca56ce
Eduardo de Paula <eduardodepaulatrabalho2@gmail.com>
2025-11-20 12:14:00 -0300

    feat: Exercício TS - Prática com tipagem de objetos

--- CursoDev-TypeScript/tipagem-de-objetos/index.js
+++ CursoDev-TypeScript/tipagem-de-objetos/index.js
@@ -3,3 +3,40 @@ function showCustomerInfo(custumer) {
     console.log("A idade do cliente \u00E9: ".concat(custumer.age));
 }
 showCustomerInfo({ name: "João", age: 11 });
+function mostrarProduto(produto) {
+    console.log("Produto: ".concat(produto.nome));
+    console.log("Pre\u00E7o: ".concat(produto.preco));
+    console.log("Em estoque: ".concat(produto.emEstoque));
+}
+mostrarProduto({ nome: "Camiseta", preco: 69.99, emEstoque: false });
+function descricaoCarro(descricao) {
+    console.log("A marca do carro \u00E9: ".concat(descricao.marca));
+    console.log("O ano: ".concat(descricao.marca));
+    console.log("Cor: ".concat(descricao.cor));
+}
+descricaoCarro({ marca: "Hilux", ano: 2019 });
+descricaoCarro({ marca: "Palio", ano: 2014, cor: "Vermelho" });
+//Versão corrigida
+function descricaoCarro2(descricao) {
+    console.log("A marca do carro \u00E9: ".concat(descricao.marca));
+    console.log("O ano: ".concat(descricao.ano));
+    if (descricao.cor) {
+        console.log("Cor: ".concat(descricao.cor));
+    }
+    else {
+        console.log("Cor não informada");
+    }
+}
+descricaoCarro2({ marca: "Hilux", ano: 2019 });
+descricaoCarro2({ marca: "Palio", ano: 2014, cor: "Vermelho" });
+function mostrarPessoa(pessoa) {
+    console.log("Nome: ".concat(pessoa.nome));
+    console.log("O Endere\u00E7o \u00E9: rua: ".concat(pessoa.endereco.rua, " e o numero \u00E9: ").concat(pessoa.endereco.numero));
+}
+mostrarPessoa({ nome: "Felipe", endereco: { rua: "Tal, Tal", numero: 231 } });
+//Forma mais organizada
+function mostrarPessoa2(pessoa) {
+    console.log("Nome: ".concat(pessoa.nome));
+    console.log("Endere\u00E7o: ".concat(pessoa.endereco.rua, ", n\u00BA ").concat(pessoa.endereco.numero));
+}
+mostrarPessoa2({ nome: "Felipe", endereco: { rua: "Tal, Tal", numero: 231 } });
--- CursoDev-TypeScript/tipagem-de-objetos/index.ts
+++ CursoDev-TypeScript/tipagem-de-objetos/index.ts
@@ -9,3 +9,127 @@ function showCustomerInfo(custumer : Custumer) {
 }
 
 showCustomerInfo({name: "João", age: 11});
+
+
+
+//< ------------- Exercícios --------------- >
+
+/* 🔹 Exercício 1 — Tipar um objeto simples
+
+Crie um tipo chamado Produto com as propriedades:
+nome (string)
+preco (number)
+emEstoque (boolean)
+
+Depois, crie uma função mostrarProduto que recebe um objeto desse tipo e mostra suas informações no console.
+
+💡 Dica:
+Use um type e depois crie um objeto seguindo esse tipo.
+A função deve aceitar um único parâmetro, que é o objeto. */
+
+type Produto = {
+    nome: string;
+    preco: number;
+    emEstoque: boolean;
+}
+
+function mostrarProduto (produto: Produto) {
+    console.log(`Produto: ${produto.nome}`);
+    console.log(`Preço: ${produto.preco}`);
+    console.log(`Em estoque: ${produto.emEstoque}`);
+}
+
+mostrarProduto({nome: "Camiseta", preco: 69.99, emEstoque: false});
+
+
+/* 🔹 Exercício 2 — Tipo com propriedades opcionais
+
+Crie um tipo Carro com:
+marca (string)
+ano (number)
+cor? (string — opcional)
+
+Crie uma função descricaoCarro que retorna um texto descrevendo o carro.
+
+💡 Dica:
+Propriedade opcional usa o símbolo ?
+Dentro da função, antes de usar cor, verifique se ela existe. */
+
+type Carro = {
+    marca: string;
+    ano: number;
+    cor?: string;
+}
+
+function descricaoCarro(descricao: Carro) {
+    console.log(`A marca do carro é: ${descricao.marca}`);
+    console.log(`O ano: ${descricao.marca}`);
+    console.log(`Cor: ${descricao.cor}`);
+}
+
+descricaoCarro({marca: "Hilux", ano: 2019});
+descricaoCarro({marca: "Palio", ano: 2014, cor: "Vermelho"});
+
+//Versão corrigida
+function descricaoCarro2(descricao: Carro) {
+    console.log(`A marca do carro é: ${descricao.marca}`);
+    console.log(`O ano: ${descricao.ano}`);
+
+    if (descricao.cor) {
+        console.log(`Cor: ${descricao.cor}`);
+    } else {
+        console.log("Cor não informada");
+    }
+}
+
+descricaoCarro2({ marca: "Hilux", ano: 2019 });
+descricaoCarro2({ marca: "Palio", ano: 2014, cor: "Vermelho" });
+
+
+/* 🔹 Exercício 3 — Tipos dentro de tipos
+
+Crie dois tipos:
+
+Endereco
+rua (string)
+numero (number)
+
+Pessoa
+nome (string)
+endereco (Endereco)
+
+Crie uma função mostrarPessoa que recebe uma Pessoa e mostra o nome e a rua onde ela mora.
+
+💡 Dica:
+Você vai usar um tipo dentro de outro, assim:
+
+type Pessoa = {
+    nome: string;
+    endereco: Endereco;
+}
+ */
+
+type Endereco = {
+    rua: string;
+    numero: number
+}
+
+type Pessoa = {
+    nome: string;
+    endereco: Endereco;
+}
+
+function mostrarPessoa(pessoa: Pessoa) {
+    console.log(`Nome: ${pessoa.nome}`);
+    console.log(`O Endereço é: rua: ${pessoa.endereco.rua} e o numero é: ${pessoa.endereco.numero}`);
+}
+
+mostrarPessoa({nome: "Felipe", endereco: {rua: "Tal, Tal", numero: 231}});
+
+//Forma mais organizada
+function mostrarPessoa2(pessoa: Pessoa) {
+    console.log(`Nome: ${pessoa.nome}`);
+    console.log(`Endereço: ${pessoa.endereco.rua}, nº ${pessoa.endereco.numero}`);
+}
+
+mostrarPessoa2({nome: "Felipe", endereco: {rua: "Tal, Tal", numero: 231}});
