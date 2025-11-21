3701e5cb498cc60cc670081bfc8d932d211907c3
Eduardo de Paula <eduardodepaulatrabalho2@gmail.com>
2025-11-21 20:22:44 -0300

    feat: Exercício TS - Práticando parâmetros opcionais

--- CursoDev-TypeScript/propriedades-parametros-opcionais/index.js
+++ CursoDev-TypeScript/propriedades-parametros-opcionais/index.js
@@ -15,3 +15,64 @@ function printName(firstName, lastName) {
 }
 ;
 printName("João");
+function printLivro(livro) {
+    console.log("T\u00EDtulo do livro: ".concat(livro.titulo));
+    console.log("Autor: ".concat(livro.autor));
+    console.log("Ano Publica\u00E7\u00E3o: ".concat(livro.anoPublicacao));
+    //Caso precise verificar se é undefined ---> if (livro.edicao !== undefined)
+    if (livro.edicao) {
+        console.log("Edi\u00E7\u00E3o: ".concat(livro.edicao));
+    }
+    else {
+        console.log("Edição não informada");
+    }
+}
+printLivro({ titulo: "Harry Potter", autor: "J. K.", anoPublicacao: 2001 });
+//Versão encurtada/melhorada
+function printLivro2(livro) {
+    var _a;
+    console.log("T\u00EDtulo: ".concat(livro.titulo));
+    console.log("Autor: ".concat(livro.autor));
+    console.log("Ano: ".concat(livro.anoPublicacao));
+    console.log("Edi\u00E7\u00E3o: ".concat((_a = livro.edicao) !== null && _a !== void 0 ? _a : "Não informada"));
+}
+printLivro2({ titulo: "Percy Jackson", autor: "Rick Riordan", anoPublicacao: 2005, edicao: 1 });
+/* 📝 Exercício 2 — Parâmetro opcional na função
+
+Crie uma função chamada saudar que receba:
+nome (string)
+saudacao? (string — opcional)
+
+Se a saudação existir → use ela.
+Se não existir → use "Olá" como padrão.
+
+Exemplos esperados (não faça o código agora, só para entender o que deve acontecer):
+saudar("Ana") → "Olá, Ana"
+saudar("João", "Bom dia") → "Bom dia, João" */
+function saudar(nome, saudacao) {
+    if (saudacao !== undefined) {
+        console.log("".concat(saudacao, ", ").concat(nome));
+    }
+    else {
+        console.log("Ol\u00E1, ".concat(nome));
+    }
+}
+saudar("João");
+saudar("Ana", "Bom dia, ");
+//Versão curta/melhorada 
+function saudar2(nome, saudacao) {
+    if (saudacao === void 0) { saudacao = "Olá"; }
+    console.log("".concat(saudacao, ", ").concat(nome));
+}
+saudar2("Diogo"); // Usa "Olá"
+saudar2("Marcos", "Bom dia"); // Usa "Bom dia"
+function printFilme(infoFilme) {
+    var _a, _b;
+    console.log("Filme: ".concat(infoFilme.nome));
+    console.log("Diretor: ".concat(infoFilme.diretor));
+    console.log("Ano: ".concat((_a = infoFilme.ano) !== null && _a !== void 0 ? _a : "Não informado"));
+    console.log("Genero: ".concat((_b = infoFilme.genero) !== null && _b !== void 0 ? _b : "Não informado"));
+}
+;
+printFilme({ nome: "Transformers", diretor: "Michael Bay" });
+printFilme({ nome: "Piratas do Caribe", diretor: "Gore Verbinski", ano: 2003, genero: "Aventura" });
--- CursoDev-TypeScript/propriedades-parametros-opcionais/index.ts
+++ CursoDev-TypeScript/propriedades-parametros-opcionais/index.ts
@@ -37,3 +37,113 @@ printName("João");
 
 
 
+// < ----------------- Exercícios -------------------- >
+
+/* 📝 Exercício 1 — Propriedade opcional em objeto
+
+Crie um type chamado Livro com:
+título (string)
+autor (string)
+anoPublicacao (number)
+edição? (number — opcional)
+
+Depois, crie uma função que receba um objeto Livro e escreva no console as informações.
+Se a edição existir, mostre também. Se não existir, ignore. */
+
+type Livro = {
+    titulo: string;
+    autor: string;
+    anoPublicacao: number;
+    edicao?: number;
+}
+
+function printLivro(livro: Livro) {
+    console.log(`Título do livro: ${livro.titulo}`);
+    console.log(`Autor: ${livro.autor}`);
+    console.log(`Ano Publicação: ${livro.anoPublicacao}`);
+
+    //Caso precise verificar se é undefined ---> if (livro.edicao !== undefined)
+    if(livro.edicao){
+        console.log(`Edição: ${livro.edicao}`);
+    } else {
+        console.log("Edição não informada");
+    }
+
+}
+
+printLivro({titulo: "Harry Potter", autor: "J. K.", anoPublicacao: 2001});
+
+
+//Versão encurtada/melhorada
+function printLivro2(livro: Livro) {
+    console.log(`Título: ${livro.titulo}`);
+    console.log(`Autor: ${livro.autor}`);
+    console.log(`Ano: ${livro.anoPublicacao}`);
+    console.log(`Edição: ${livro.edicao ?? "Não informada"}`);
+}
+
+printLivro2({titulo: "Percy Jackson", autor: "Rick Riordan", anoPublicacao: 2005, edicao: 1});
+
+
+/* 📝 Exercício 2 — Parâmetro opcional na função
+
+Crie uma função chamada saudar que receba:
+nome (string)
+saudacao? (string — opcional)
+
+Se a saudação existir → use ela.
+Se não existir → use "Olá" como padrão.
+
+Exemplos esperados (não faça o código agora, só para entender o que deve acontecer):
+saudar("Ana") → "Olá, Ana"
+saudar("João", "Bom dia") → "Bom dia, João" */
+
+function saudar(nome: string , saudacao?: string) {
+    if (saudacao !== undefined) {
+        console.log(`${saudacao}, ${nome}`);
+    } else {
+        console.log(`Olá, ${nome}`);
+    }
+}
+
+saudar("João");
+saudar("Ana", "Bom dia, ");
+
+//Versão curta/melhorada 
+function saudar2(nome: string, saudacao: string = "Olá") {
+    console.log(`${saudacao}, ${nome}`);
+}
+
+saudar2("Diogo");                 // Usa "Olá"
+saudar2("Marcos", "Bom dia");       // Usa "Bom dia"
+
+
+/* 📝 Exercício 3 — Objeto com 2 propriedades opcionais
+
+Crie um type Filme com:
+nome (string)
+diretor (string)
+ano? (number — opcional)
+genero? (string — opcional)
+
+Depois crie uma função que receba um Filme e:
+sempre mostre nome e diretor
+mostre o ano apenas se existir
+mostre o gênero apenas se existir */
+
+type Filme = {
+    nome: string;
+    diretor: string;
+    ano?: number;
+    genero?: string;
+};
+
+function printFilme(infoFilme: Filme) {
+    console.log(`Filme: ${infoFilme.nome}`);
+    console.log(`Diretor: ${infoFilme.diretor}`);
+    console.log(`Ano: ${infoFilme.ano ?? "Não informado"}`);
+    console.log(`Genero: ${infoFilme.genero ?? "Não informado"}`);
+};
+
+printFilme({nome: "Transformers", diretor: "Michael Bay"});
+printFilme({nome: "Piratas do Caribe", diretor: "Gore Verbinski", ano: 2003, genero: "Aventura"});
