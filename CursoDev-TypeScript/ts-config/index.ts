4ab306ceb0c9b2f14a94c67e4a8860beb5f15748
Eduardo de Paula <eduardodepaulatrabalho2@gmail.com>
2025-11-25 19:56:19 -0300

    feat: Conteúdo TS - TS Config: Configurando o TS dos nossos projetos

---
+++ CursoDev-TypeScript/ts-config/index.ts
@@ -0,0 +1,12 @@
+// Com o verificar de implicto any no true
+
+function printAge(age) {
+    console.log(age);
+    
+}
+
+//O certo a se fazer
+function printAge2(age: number) {
+    console.log(age);
+    
+}
