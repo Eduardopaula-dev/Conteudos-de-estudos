ffe603634611c866d9a42d175d917c91b57588d7
Eduardo de Paula <eduardodepaulatrabalho2@gmail.com>
2025-12-05 20:24:29 -0300

    feat: Conteúdo React - JSX na prática

---
+++ CursoDev-React/pratica-jsx-tsx/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  const name = "Ricardo"
+
+  return (
+    <>
+      <h1>Hello, {name}</h1>
+      <p>Seja Bem-vindo</p>
+    </>
+  )
+}
+
+export default App
