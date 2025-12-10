401748559100d52c5705199e621bf436b1be0970
Eduardo de Paula <eduardodepaulatrabalho2@gmail.com>
2025-12-09 21:23:40 -0300

    feat: Conteúdo React - Props Children

---
+++ CursoDev-React/props-children/src/App.tsx
@@ -0,0 +1,18 @@
+import Card from './components/Card'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <Card>
+      <h2>Título do cartão</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam laborum exercitationem voluptates, sit in consectetur dicta nulla quidem repudiandae ex praesentium minus eaque. Aspernatur ad culpa consectetur cupiditate dolore ducimus?</p>
+     </Card>
+     <Card/>
+    </>
+  )
+}
+
+export default App
